b52c61e38dd9d76368d3a8455559681b4205ff8b
zoeyer <zhouyiyayo@gmail.com>
2026-05-17 15:15:09 +0800

    做成桌面图标

---
+++ sw.js
@@ -0,0 +1,45 @@
+/* ======== 计件薪资 - Service Worker ======== */
+const CACHE = 'piecewage-v1';
+const URLS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './appicon.png',
+  'https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(URLS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).then(function(res) {
+        return caches.open(CACHE).then(function(cache) {
+          cache.put(e.request, res.clone());
+          return res;
+        });
+      }).catch(function() {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
